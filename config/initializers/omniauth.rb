f54c9390b2364c5f207e24166bb789942c9e47a5
Nurettin Şenyer <seyyah>
2012-06-29 19:33:01 +0300

    .

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  configure do |config|
-    config.path_prefix = '/auth'
-  end
+#  configure do |config|
+#    config.path_prefix = '/auth'
+#  end
 
   provider :facebook, '251388158310534', '453fdb776a9bfed8d8350c0ab897a22a' 
   #provider :google, 'emineker.net', 'PtyLaRmzLQ1LodMJVrmYdt3b'
